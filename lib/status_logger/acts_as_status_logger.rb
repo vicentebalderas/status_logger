a3b0a2cc793d9c635829ef9857b872036e0a8c7a
Vicente Balderas <vbm1191@gmail.com>
2020-01-24 16:01:54 -0600

    Add business time dependency

---
+++ lib/status_logger/acts_as_status_logger.rb
@@ -0,0 +1,59 @@
+module StatusLogger
+  module ActsAsStatusLogger
+    extend ActiveSupport::Concern
+
+    included do
+      after_save :save_status_changes
+
+      protected
+
+        def save_status_changes
+          BusinessTime::Config.beginning_of_workday = '8:00 am'
+          BusinessTime::Config.end_of_workday = '7:00 pm'
+
+          binding.pry
+
+          to = 'pending'
+
+          last_status_change_log = self.status_change_logs.last
+
+          if last_status_change_log.present?
+            from = last_status_change_log.to
+            started_at = last_status_change_log.transition_ended_at
+          else
+            from = nil
+            started_at = self.created_at
+          end
+
+          ended_at = Time.current
+
+          elapsed_time = started_at.to_time - ended_at
+
+          elapsed_business_time_in_seconds = started_at.business_time_until(transition_ended_at).to_i
+
+          self.status_change_logs
+              .create(status_attribute: 'status',
+                      from: from,
+                      to: to,
+                      transition_started_at: started_at,
+                      transition_ended_at: ended_at,
+                      elapsed_time_in_seconds: elapsed_time,
+                      elapsed_business_time_in_seconds: elapsed_business_time_in_seconds)
+        end
+    end
+
+    class_methods do
+      def acts_as_status_logger(options = {})
+        @status_attributes = options[:status_attributes]
+      end
+    end
+  end
+end
+
+# class StatusChangeLog < ApplicationRecord
+#   belongs_to :status_loggeable, polymorphic: true
+# end
+
+# class JobApplication < ApplicationRecord
+#   has_many :status_change_logs, as: :status_loggeable
+# end
